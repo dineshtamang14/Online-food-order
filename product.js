322d0e511b786905e5a0876f0fe11e95f8a8280c
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-10-25 16:22:27 +0530

    v1

---
+++ product.js
@@ -0,0 +1,65 @@
+const products = [
+  {
+    id: 1,
+    name: "Burgurs",
+    price: 150,
+    stock: 100,
+    discription: "Portobello Mushroom Burgers.",
+  },
+  {
+    id: 2,
+    name: "Sandwiches",
+    price: 190,
+    stock: 100,
+    discription: "Roast Beef Sandwich.",
+  },
+  {
+    id: 3,
+    name: "Frankies & rolls",
+    price: 200,
+    stock: 100,
+    discription: "Paneer Frankie.",
+  },
+  {
+    id: 4,
+    name: "cupcakes",
+    price: 200,
+    stock: 100,
+    discription: "Coconut Cupcakes.",
+  },
+  {
+    id: 5,
+    name: "Sweets",
+    price: 250,
+    stock: 100,
+    discription: "Shrikhand.",
+  },
+  {
+    id: 6,
+    name: "Pakodas",
+    price: 300,
+    stock: 100,
+    discription: "Thika Pakodas.",
+  },
+  {
+    id: 7,
+    name: "Breakfast",
+    price: 340,
+    stock: 100,
+    discription: "Coconut Cupcakes.",
+  },
+  {
+      id: 8,
+      name: "ch"
+  }
+];
+
+
+// products section
+
+let productsInput = document.querySelector(".products__gallery");
+productsInput.innerHTML = `
+    products.forEach((item)=>{
+
+    });
+  `
